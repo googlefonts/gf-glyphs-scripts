cc4c24b8302bb496b87e7dd352fe82076eec02a1
Marc Foley <m.foley.88@gmail.com>
2017-08-02 17:30:22 +0100

    Custom runner added which is type designer friendly

---
+++ Google Fonts/result.py
@@ -0,0 +1,40 @@
+from unittest import TestResult
+
+
+class GlyphsTestResult(TestResult):
+    """Customised Test Results which are readable to type designers"""
+    def addFailure(self, test, err):
+        """Called when an error has occurred. 'err' is a tuple of values as
+        returned by sys.exc_info()."""
+        # print(dir(test), test.shortDescription())
+        title = test.shortDescription()
+        self.failures.append((title, (self._exc_info_to_string(err, test))))
+        self._mirrorOutput = True
+
+    def _exc_info_to_string(self, err, test):
+        """Converts a sys.exc_info()-style tuple of values into a string."""
+        exctype, value, tb = err
+        # Skip test runner traceback levels
+        while tb and self._is_relevant_tb_level(tb):
+            tb = tb.tb_next
+
+        if exctype is test.failureException:
+            # Skip assert*() traceback levels
+            length = self._count_relevant_tb_levels(tb)
+            msgLines = value
+        else:
+            msgLines = value
+
+        if self.buffer:
+            output = sys.stdout.getvalue()
+            error = sys.stderr.getvalue()
+            print error
+            if output:
+                if not output.endswith('\n'):
+                    output += '\n'
+                msgLines.append(STDOUT_LINE % output)
+            if error:
+                if not error.endswith('\n'):
+                    error += '\n'
+                msgLines.append(STDERR_LINE % error)
+        return ''.join(msgLines)
---
+++ Google Fonts/runner.py
@@ -0,0 +1,52 @@
+from unittest import TextTestRunner
+from result import GlyphsTestResult
+
+
+class GlyphsTestRunner(TextTestRunner):
+    """Simple test runner which is suitable for type designers"""
+
+    description = """Google Fonts QA
+~~~~~~~~~~~~~~~
+
+Tests to ensure .glyphs file follow the Google Fonts specification.
+
+The Google Fonts specification:
+https://github.com/googlefonts/gf-docs/blob/master/ProjectChecklist.md
+
+Quick start guide for Glyphs:
+https://github.com/googlefonts/gf-docs/blob/master/QuickStartGlyphs.md
+
+
+A fix script which will attempt to update the font to match the
+specification can be found in the same directory as this script.
+Google Fonts > Fix fonts for GF spec
+
+    """
+    def run(self, test):
+        result = GlyphsTestResult()
+        test(result)
+        all_tests = result.testsRun
+        failed_tests = len(result.failures)
+        passed_tests = all_tests - failed_tests
+        result.printErrors()
+        run = result.testsRun
+        self.stream.writeln(self.description)
+        self.stream.writeln("TESTS RUN: %s | PASSED: %s | FAILED: %s\n" % (
+            all_tests,
+            passed_tests,
+            failed_tests
+            )
+        )
+        
+        if not result.wasSuccessful():
+            for failed_test in result.failures:
+                title, fix_text = failed_test
+                self.stream.writeln('=' * 80)
+                self.stream.writeln('FAIL: ' + title)
+                self.stream.writeln('-' * 80)
+                self.stream.writeln(''.join(fix_text))
+                self.stream.writeln('\n')
+        else:
+            self.stream.writeln('PASSED. No Errors')
+
+        return result
